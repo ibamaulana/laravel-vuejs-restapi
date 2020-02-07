2fd64dc212cc7fe915db8062c39b6eeb602c7c3b
ibam <ibamaulana@gmail.com>
2020-02-07 20:24:39 +0700

    update

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,16 +6,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Libs CSS -->
+    <link rel="stylesheet" href="./assets/fonts/Feather/feather.css">
     <link rel="stylesheet" href="./assets/libs/flickity/dist/flickity.min.css" defer>
     <link rel="stylesheet" defer href="./assets/libs/flickity-fade/flickity-fade.css">
     <link rel="stylesheet" defer href="./assets/libs/aos/dist/aos.css">
     <link rel="stylesheet" defer href="./assets/libs/jarallax/dist/jarallax.css">
     <link rel="stylesheet" defer href="./assets/libs/highlightjs/styles/vs2015.css">
     <link rel="stylesheet" defer href="./assets/libs/@fancyapps/fancybox/dist/jquery.fancybox.min.css">
-    <link href="{{ URL::asset('assets/css/icon.css')}}" rel="stylesheet" type="text/css" defer>
+    <link href="{{ URL::asset('assets/css/icons.css')}}" rel="stylesheet" type="text/css" defer>
 
     <!-- Theme CSS -->
-    <link rel="stylesheet" href="./assets/css/themes.min.css">
+    <link rel="stylesheet" href="./assets/css/theme.min.css">
     <link rel="stylesheet" href="./assets/css/custom.css">
 
     <link rel="icon" href="/assets/images/niaga-icon.png" type="image/x-icon">
